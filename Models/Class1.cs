574e9bcd34663f3b806821fedcd45d6c0c9c5295
Григорий <gregorryger@gmail.com>
2025-09-23 09:36:45 +0700

    Добавление Models

---
+++ Models/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace Models
+///<summary>
+///Представление модели игры, включающее ID, Name, Genre, Rating
+///</summary>
+{
+    public class Class1
+    {
+        ///<summary>
+        ///ID игры
+        ///</summary>
+        public int Id { get; set; }
+
+        ///<summary>
+        ///Name игры
+        /// </summary>
+        public string Name { get; set; }
+
+        ///<summary>
+        ///Genre игры
+        /// </summary>
+        public string Genre { get; set; }
+
+        ///<summary>
+        ///Rating игры(1-10)
+        ///</summary>
+        public double Rating { get; set; }
+    }
+}
